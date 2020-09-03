ad163488ea76c248372e358e93a06dbed823e04e
wangm7 <wangm7@mail.uc.edu>
2020-09-03 00:21:26 -0400

    finished test 2.string
     return str.split("".resers().join(""));//one line
     return str.toUpperCase();
     return str.split(splitAt);

--- src/2.strings.js
+++ src/2.strings.js
@@ -4,24 +4,27 @@ function reverseString(str) {
   /**
    * can be achieved by combining: 
    *    1. string.prototype.split
+   *   let strArr = str.split(""); //split string
    *    2. array.prototype.reverse
+   *   let rev = strArr.resers(); // reverse 
    *    3. array.prototype.join
-   * 
+   *   let reverseString = rev.join(""); //joining as an sentense
+
    * Can even be done on one line
    */
-  return ;
+  return str.split("".resers().join(""));//one line
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
-  return ;
+  return str.toUpperCase();
 }
 
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
-  return ;
+  return str.split(splitAt);
 }
 module.exports = {
   reverseString,
