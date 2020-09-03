9a027dbca5d160e59ed46a0b64e946aba3d23ccb
wangm7 <wangm7@mail.uc.edu>
2020-09-03 00:02:27 -0400

    finished test 1.numbers
    add,
    sub,
    multi,
    .toFixed(2)

--- src/1.numbers.js
+++ src/1.numbers.js
@@ -1,16 +1,20 @@
 //add: Should be able to add numbers
 function add (num1, num2) {
-  return ;
+  return num1 + num2;
 }
 
 //subtract: Should be able to subtract numbers
 function subtract (num1, num2) {
-  return ;
+  if (num1 > num2){
+    return num1 - num2;
+  } else{
+    return num2 - num1;
+  }
 }
 
 //multiply: Should be able to multiply with precision
 function multiply(num1, num2) {
-  return ;
+  return num1 * num2;
 }
 
 //parseInt: should use parseInt correctly to convert strings to integer numbers
@@ -21,7 +25,7 @@ function parseInt (str) {
 //addAndReturn2DecimalPlaces: should return a number value, rounded to 2 decimal places
 // Example: 1.23453 + 5.37873 should return 6.61 and the return type should be a number
 function addAndReturn2DecimalPlaces(num1, num2) {
-  return ;
+  return result = Number((num1+num2).toFixed(2));;
 }
 
 module.exports  = {
